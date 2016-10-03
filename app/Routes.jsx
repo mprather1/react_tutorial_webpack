f9fa9ee878c3c17a0d372461a94a2b5397e4d306
Michael Prather <mprather1@gmail.com>
2016-10-02 20:42:29 -0500

    kinda works

--- app/Routes.jsx
+++ app/Routes.jsx
@@ -2,11 +2,19 @@ var React = require("react");
 var ReactRouter = require("react-router");
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
+
+// var CreateHistory = require("react-router/lib/hashHistory");
+
+// var History = new CreateHistory({
+//   queryKey: false  
+// });
+
 var Base = require("./components/Base.jsx");
 var Page1 = require("./components/Page1.jsx");
 var Page2 = require("./components/Page2.jsx");
 
 var Routes = (
+  // <Router history={History}>
   <Router>
     <Route path="/" component={Base}>
       <Route path="/page1" component={Page1} />
